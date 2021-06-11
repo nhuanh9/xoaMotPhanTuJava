308d09d963b687d78e9a67b09d85c6f45fae8c8e
NA <chopperlouis@gmail.com>
2021-06-11 11:08:26 +0700

    thêm tostring ở student thành công

--- src/demo/Student.java
+++ src/demo/Student.java
@@ -22,4 +22,12 @@ public class Student extends People {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                '}';
+    }
 }
+
